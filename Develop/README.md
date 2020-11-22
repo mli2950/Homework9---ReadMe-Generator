a491a84789981f80ed72f07fea707633acd5b122
Michael Li <Mli2950@gmail.com>
2020-11-22 14:15:03 -0700

    Installed more dependancies, generated test README

---
+++ Develop/README.md
@@ -0,0 +1,38 @@
+# README Generator
+
+
+        ## Description
+
+        Test
+
+
+        ## Table of Contents
+
+        * [Installation](#installation)
+
+        * [Usage](#usage)
+
+        * [Credits](#credits)
+
+        * [License](#license)
+
+
+    
+        ## Installation
+
+        test
+
+
+        ## Usage
+
+        test
+
+
+        # Credits
+
+        test
+
+
+        # License
+
+        test
--- Develop/index.js
+++ Develop/index.js
@@ -1,16 +1,66 @@
+const fs = require("fs")
+const inquirer = require("inquirer")
+
 // array of questions for user
-const questions = [
+inquirer
+    .prompt([
+        {
+            type: "input",
+            message: "What is the title of your project?",
+            name: "title"
+        },
+        {
+            type: "input",
+            message: "Enter your project description",
+            name: "description",
+        },
+        {
+            type: "input",
+            message: "Enter installation instructions",
+            name: "installation",
+        },
+        {
+            type: "input",
+            message: "Enter Usage instructions",
+            name: "usage",
+        },
+        {
+            type: "input",
+            message: "Who do you want to give credit to",
+            name: "credits",
+        },
+        {
+            type: "input",
+            message: "Any licenses?",
+            name: "license",
+        }
+    ])
 
-];
+    .then((data) => {
+        fs.writeFile("README.md",
+        `# ${data.title}\n\n
+        ## Description\n
+        ${data.description}\n\n
+        ## Table of Contents\n
+        * [Installation](#installation)\n
+        * [Usage](#usage)\n
+        * [Credits](#credits)\n
+        * [License](#license)\n\n
+    
+        ## Installation\n
+        ${data.installation}\n\n
+        ## Usage\n
+        ${data.usage}\n\n
+        # Credits\n
+        ${data.credits}\n\n
+        # License\n
+        ${data.license}`
+    , 
+    (err) => err ? console.log(err) : console.log("README successfully generated"))
 
-// function to write README file
-function writeToFile(fileName, data) {
-}
 
-// function to initialize program
-function init() {
+    })
 
-}
+    
 
-// function call to initialize program
-init();
+
